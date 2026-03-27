86a39af1e249bc56da9f5f8f7bb80b01bb7a4b0c
chinawin <chinawin47@gmail.com>
2026-03-27 13:11:32 +0700

    submit assignment

--- ex-5.js
+++ ex-5.js
@@ -7,6 +7,12 @@ const students = [
 
 function getAverageStudentScore(students) {
   // Start coding here
+  const totalScore = students.reduce(function sumScore(sum, student) {
+    return sum + student.score;
+  }, 0);
+  return totalScore / students.length;
 }
 
-getAverageStudentScore(students); // Output: 87.5
+const student = getAverageStudentScore(students);
+console.log(student);
+// Output: 87.5
